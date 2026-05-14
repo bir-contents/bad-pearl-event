4d3cb2f7791cf8fabf23b99ff4ff1f51da2f0316
bir-contents <dynnn@bir.co.kr>
2026-05-14 13:04:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAD_PEARL_CONFIG = {
-  SUPABASE_URL: "https://ebyqloadxbgjbcwxwdfv.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_M9QtXId6tuba_IH_kHpR8A_OK0m-0SG"
+  SUPABASE_URL: "https://ebyqloadxbgjbcwxwdfv.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVieXFsb2FkeGJnamJjd3h3ZGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTg2NzIsImV4cCI6MjA5NDI5NDY3Mn0.pEBkKRMz_nvTR3v9pA8AloEzWHHQ7aFxn21is1ruOKs"
 };
